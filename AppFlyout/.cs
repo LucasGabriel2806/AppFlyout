4856f461801c6fc982be0c5e8d1d4b2ff471d79c
Lucas Gabriel <pintolucas996@gmail.com>
2023-09-22 14:36:49 -0300

    Primeiro commit

---
+++ AppFlyout/.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFlyout
+{
+    internal class _
+    {
+    }
+}
---
+++ AppFlyout/FlyoutItemPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFlyout
+{
+    class FlyoutItemPage
+    {
+        /**
+         * Detalhes que eu quero ver na minha FlyoutPage
+         */
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+
+        /**
+         * Onde armazenamos nossa Page
+         */
+        public Type TargetPage { get; set; }
+
+    }
+}
